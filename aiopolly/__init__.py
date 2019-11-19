e04636ea9657afccdd05cd7d0c04b3239c062eaa
A. Antonitis <arant@mit.edu>
2019-11-19 16:39:47 -0500

    Implemented 'engine' parameter. (#1)
    * Implemented 'engine' parameter.
    
    * Update aiopolly/__init__.py
    
    Changed API date.
    
    Co-Authored-By: Arseny Boykov <36469655+MrMrRobat@users.noreply.github.com>

--- aiopolly/__init__.py
+++ aiopolly/__init__.py
@@ -12,5 +12,5 @@ __all__ = [
     'utils'
 ]
 
-__version__ = '0.1.1'
-__api_date__ = datetime.date(2018, 8, 2)  # August 2, 2018
+__version__ = '0.2.1'
+__api_date__ = datetime.date(2019, 7, 30)  # July 30, 2019
